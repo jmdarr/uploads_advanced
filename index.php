daa93126700e1b2d081204064ae452d09a83b944
Jeremy Darr <jeremy.darr@gmail.com>
2013-10-06 09:05:16 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+### PSEUDOCODE ###
+# start session
+# if not logged in, show header with login link
+# if logged in, show header with username/relative links
+# show public page
+# show footer
+
+?>
