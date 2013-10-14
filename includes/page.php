3056aae4624c6439154e3de2fa831579179a1669
Jeremy Darr <jeremy.darr@gmail.com>
2013-10-13 19:37:48 -0700

    Working on getting page class to output templates.

--- includes/page.php
+++ includes/page.php
@@ -3,9 +3,9 @@
 class page {
     
     public function display_template(string template) {
-        
         # force check on file
         # do not include anything outside of template
+        if(is_sane_path($template)) { }
     }
 }
 
